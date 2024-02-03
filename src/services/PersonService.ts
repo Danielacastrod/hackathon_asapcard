3251fc811b2e77815dce7e2f130b44ce5acdb13f
Walter Fernandes Netto <40149590+Walttinho@users.noreply.github.com>
2024-02-03 19:13:28 -0300

    Revert "chore: initialize project structure"

--- src/services/PersonService.ts
+++
@@ -1,8 +0,0 @@
-import { Person } from '../models';
-
-export const PersonService = {
-  findById: async (id: string) => {
-    const exemplo = await Person.findByPk(id);
-    return exemplo;
-  },
-};
