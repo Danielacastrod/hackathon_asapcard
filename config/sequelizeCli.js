2fa10b589b7afb23a336856ef505292b9a88563f
Walter Fernandes Netto <40149590+Walttinho@users.noreply.github.com>
2024-02-03 22:39:29 -0300

    hotfix: sequalize extension (#11)
    * hotfix: refatorando a main.
    
    * hotfix: sequilize estension

---
+++ config/sequelizeCli.js
@@ -0,0 +1,11 @@
+module.exports = {
+  development: {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: '5432',
+    database: 'hackathon_development',
+    username: 'postgres',
+    password: '123456',
+  },
+};
+  
